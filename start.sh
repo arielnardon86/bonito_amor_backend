e96df23f60be0ccba77e2750fc56ad9ab46afa57
Ariel Nardon <arinardon@Ariels-MacBook-Pro.local>
2025-10-19 18:22:47 -0300

    estilos backend

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Aplicar las migraciones de la base de datos
+# Aplicar las migraciones de la base de datos (sin preguntar)
 echo "Aplicando migraciones de la base de datos..."
-python manage.py migrate
+python manage.py migrate --no-input
+
+# Recolectar archivos estáticos (sin preguntar)
+echo "Recolectando archivos estáticos..."
+python manage.py collectstatic --no-input
 
 # Iniciar el servidor Gunicorn
 echo "Iniciando servidor Gunicorn..."
